1df8c5d7622d461765bcd8de2d59303636f36810
Healivas <ya.healis@yandex.ru>
2025-06-22 11:13:12 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -959,3 +959,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+window.addEventListener(
+  "wheel",
+  function (e) {
+    if (e.ctrlKey) {
+      e.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
+window.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && (e.key === "+" || e.key === "-" || e.key === "=" || e.key === "_")) {
+    e.preventDefault();
+  }
+});
